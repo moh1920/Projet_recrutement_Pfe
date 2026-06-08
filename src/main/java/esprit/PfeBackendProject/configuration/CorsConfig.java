b3d111a328009ea106296f9310d00368afcec67a
moh1920 <sayari.mohamed@esprit.tn>
2026-06-08 13:24:32 +0100

    ci: deploy to ACI via ACR

--- src/main/java/esprit/PfeBackendProject/configuration/CorsConfig.java
+++ src/main/java/esprit/PfeBackendProject/configuration/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200","https://pfe-frontend.graymoss-d46652df.francecentral.azurecontainerapps.io")
+                .allowedOrigins("http://localhost:4200","https://pfe-frontend.graymoss-d46652df.francecentral.azurecontainerapps.io","https://projet-recrutement-pfe.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
