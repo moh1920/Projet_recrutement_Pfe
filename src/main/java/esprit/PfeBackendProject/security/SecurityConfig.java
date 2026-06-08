751a25d3a8f9baf2b986e1f9c6f9c85979fed3e0
moh1920 <sayari.mohamed@esprit.tn>
2026-06-08 17:06:31 +0100

    ci: add Docker Hub to ACR sync pipeline

--- src/main/java/esprit/PfeBackendProject/security/SecurityConfig.java
+++ src/main/java/esprit/PfeBackendProject/security/SecurityConfig.java
@@ -83,7 +83,8 @@ public class SecurityConfig{
         configuration.setAllowedOrigins(Arrays.asList(
                 "http://localhost:4200",
                 "https://pfe-frontend.graymoss-d46652df.francecentral.azurecontainerapps.io",
-                "https://projet-recrutement-pfe.vercel.app"
+                "https://projet-recrutement-pfe.vercel.app",
+                "https://pfe-frontend.orangesand-21e6d03b.germanywestcentral.azurecontainerapps.io"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
