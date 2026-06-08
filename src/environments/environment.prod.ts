78034480b8258e5b6a35cb5ebf9afaa72cd05a3e
moh1920 <sayari.mohamed@esprit.tn>
2026-06-08 20:13:28 +0100

    ci: deploy to ACI via ACR

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   keycloakUrl:
-    'https://pfe-keycloak-production.up.railway.app',
+    'https://keycloak-app.orangesand-21e6d03b.germanywestcentral.azurecontainerapps.io',
   keycloakRealm: 'espritRecrutement',
   keycloakClientId: 'espritRecrutement',
   apiUrl: 'https://pfe-backend.orangesand-21e6d03b.germanywestcentral.azurecontainerapps.io',
